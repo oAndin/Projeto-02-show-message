82d56b1458990610dd66a96f9f268520c1931e13
oAndin <00andherson@gmail.com>
2023-05-26 17:59:20 -0300

    Inicio JS, CSS completo

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnEl = document.querySelector("button");
+const inputEl = document.querySelector("input");
+const messageEl = document.querySelector(".message");
+const errorEl = document.querySelector(".error");
+
+btnEl.addEventListener("click", displayMessage);
